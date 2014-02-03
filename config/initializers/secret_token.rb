fe396c583a52d53a51a6ad3a834db6accc43e29c
Mazembo <mazemb_eddy@yahoo.fr>
2014-02-03 22:10:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zando::Application.config.secret_key_base = '63e1f3ca53bd7e3530986f430da0ae90990bacdcb5bc5fa57acfe76ebc7aa4003e2235b9c68a5d74e8f3c1f09104b59a02df038eb62d2581a2c2a7dcb93365a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zando::Application.config.session_store :cookie_store, key: '_zando_session'
